ef2349565ba3632ed08b935f7a10ea1e0e149b2a
Muhammad Fadhlan Min Robby <fadhlan.mr@gmail.com>
2023-04-02 21:53:58 +0700

    fucnction-verse

--- function.py
+++ function.py
@@ -2,9 +2,6 @@ import time
 import json
 import api_call as acall
 
-lgb = 1675420911
-lgt = 1675420911
-
 class url :
     default = "a.4cdn.org"
     media = "i.4cdn.org"
@@ -31,17 +28,39 @@ def req (url, **kwargs) :
     """
     return acall.call_api(url=url, **kwargs)
 
-while True:
+def board_list(url, **kwargs) :
+    r"""Board Listing purpose, uses 4chan catalog api, return should be 
+    a dict of thread list
+    """
+
     # Make request to 4chan API
-    resp = req(url.default, board_code="vt", endpoint=endpoint.catalog)
-    # print(resp)
+    resp = req(url, **kwargs)
     data = json.loads(resp)
     threads = []
+    thread_list = []
     for item in data:
         threads.extend(item['threads'])
+        for listed in item['threads']:
+            if 'sticky' not in listed:
+                thread_list.extend({'thread_id':item['no'],'thread_posted':item['time'],'thread_update':item['last_modified']})
 
+    # turn list -> str, use encode utf to get bytes
     json_utf8 = json.dumps(threads, ensure_ascii=False)
-    print(json_utf8)
+    #
+    # threads[] json to mangodb
+    #
+
+    # return thread_list for updating purpose
+    return thread_list
+
+while True:
+    # initiate and get thread list
+    thread_loop = []
+    thread_loop.extend(board_list(url.default, board_code="vt", endpoint=endpoint.catalog))
+    
+    for threads in thread_loop :
+        time.sleep(2)
+
 
     # Wait before making next request
     time.sleep(60)
