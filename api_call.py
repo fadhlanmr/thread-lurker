a367b1e27bbe9b0cf12c8726c54e1b0976281d73
fadhlanmr <fadhlan.mr@gmail.com>
2023-09-13 15:57:23 +0700

    cleaning feature

--- api_call.py
+++ api_call.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import datetime
+from unicodedata import normalize
+from bs4 import BeautifulSoup
 
 def unix_to_gmt (unix_num) :
     date_raw = datetime.datetime.fromtimestamp(unix_num)
@@ -26,7 +28,7 @@ def call_api (url, endpoint=None, unix=None, board_code=None, thread=None) :
         response.raise_for_status()
         # below refers to requests stored status codes https://github.com/psf/requests/blob/main/requests/status_codes.py
         if response.status_code == requests.codes.ok :
-            api_output = response.text
+            api_output = response.json()
             # encoderJson = json.dumps(resp, ensure_ascii=False).encode('utf-8')
             # api_output = json.loads(encoderJson)
         else :
@@ -54,3 +56,59 @@ def list_single_thread (urls, board_2_code, thread_id, thread_last_get) :
     # clean some column
     # response_cleaned store the cleaned response
     return response
+
+## Cleaning Stuff below
+
+def clean_html(input_html: str) -> str:
+    # common uses of breaklines as whitespace
+    input_html = input_html.replace("<br>", " ")
+    cleantext = BeautifulSoup(input_html, 'html.parser')
+    # normalize, temp fix, still need to find how to change encoded(JP/any unicode char) decoded to JP char
+    finaltext = normalize('NFKC', cleantext.get_text().strip()).encode('ascii', 'replace')
+    return finaltext
+
+def if_board_list(list_board: list, clean_text: bool) -> list:
+    temp_list = list_board['boards']
+    temp_board = list()
+    for lists in temp_list:
+        temp_dict = dict()
+        for key, value in lists.items():
+            if key == 'board' :
+                temp_dict[key]=value
+            if key == 'title' :
+                vals = value.encode('utf-8')
+                temp_dict[key]=vals.decode()
+                if clean_text :
+                    temp_dict[key]=clean_html(value)
+            if key == 'meta_description':
+                vals = value.encode('utf-8')
+                temp_dict[key]=vals.decode()
+                if clean_text :
+                    temp_dict[key]=clean_html(value)
+        temp_board.append(temp_dict)
+    return temp_board
+
+def if_catalog_list(list_board: list, clean_text: bool) -> list:
+    temp_catalog = list()
+    for pages in list_board:
+        for lists in pages['threads']:
+            temp_dict = dict()
+            for key, value in lists.items():
+                if key == 'no' :
+                    temp_dict[key]=value
+                if key == 'sub' :
+                    vals = value.encode('utf-8')
+                    temp_dict[key]=vals.decode()
+                    if clean_text :
+                        temp_dict[key]=clean_html(value)
+                if key == 'com' :
+                    vals = value.encode('utf-8')
+                    temp_dict[key]=vals.decode()
+                    if clean_text :
+                        temp_dict[key]=clean_html(value)
+                if key == 'time':
+                    temp_dict[key]=value
+                if key == 'replies':
+                    temp_dict[key]=value
+            temp_catalog.append(temp_dict)
+    return temp_catalog
